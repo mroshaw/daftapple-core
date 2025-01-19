02ae786debd7edc918b0476c07138ca19194939e
Oli Ollerenshaw <oli@mroshaw.co.uk>
2025-01-19 12:28:14 +0000

    Added framework for PropertyOrderAttribute

---
+++ Runtime/Scripts/Core/Attributes/MetaAttributes/PropertyOrderAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DaftAppleGames.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class PropertyOrderAttribute : MetaAttribute
+    {
+        public int Order { get; private set; }
+
+        public PropertyOrderAttribute(int order)
+        {
+            Order = order;
+        }
+    }
+}
