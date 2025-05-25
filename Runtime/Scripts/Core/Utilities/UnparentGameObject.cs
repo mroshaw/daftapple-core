e3ffb2fbdeb4c105b231948d014b974e58be7e8b
Oli Ollerenshaw <oli@mroshaw.co.uk>
2025-05-25 14:44:13 +0100

    Added new Unparent GameObject component

---
+++ Runtime/Scripts/Core/Utilities/UnparentGameObject.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#else
+using DaftAppleGames.Attributes;
+#endif
+
+namespace DaftAppleGames.Core.Utilities
+{
+    /// <summary>
+    /// Simple component to unparent a GameObject on start or awake
+    /// </summary>
+    public class UnparentGameObject : MonoBehaviour
+    {
+        [BoxGroup("Settings")] [SerializeField] private bool unparentOnAwake;
+        [BoxGroup("Settings")] [SerializeField] private bool unparentOnStart;
+
+        private bool _isUnparented;
+
+        private void Awake()
+        {
+            if (unparentOnAwake && !_isUnparented)
+            {
+                Unparent();
+            }
+        }
+
+        private void Start()
+        {
+            if (unparentOnStart && !_isUnparented)
+            {
+                Unparent();
+            }
+        }
+
+        private void Unparent()
+        {
+            if (transform.parent != null)
+            {
+                transform.SetParent(null);
+                _isUnparented = true;
+            }
+        }
+    }
+}
